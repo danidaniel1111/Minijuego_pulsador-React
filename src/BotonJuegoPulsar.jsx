d8d854106c6eacd910dd1ccee08ec5b2da70f52b
Daniel Pérez <77322428+danidaniel1111@users.noreply.github.com>
2026-06-05 20:43:21 +0200

    Update BotonJuegoPulsar.jsx

--- src/BotonJuegoPulsar.jsx
+++ src/BotonJuegoPulsar.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 export function BotonJuegoPulsar({
   accion_puntos,
   puntos_actuales,
-  accion_tiempo_boton,
+
 }) {
   return (
     <>
@@ -13,7 +13,6 @@ export function BotonJuegoPulsar({
           className="boton_juego_pulsar"
           onClick={() => {
             accion_puntos((prev) => prev + 1);
-            accion_tiempo_boton((prev) => prev + 1);
           }}
         >
           ¡PÚLSAME!
